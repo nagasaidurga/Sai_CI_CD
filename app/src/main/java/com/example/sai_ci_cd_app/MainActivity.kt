059d5f67e53f340830596c0c4dfb634490e8af24
dambati <durga.ambati@yahooinc.com>
2024-06-17 12:57:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/sai_ci_cd_app/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.sai_ci_cd_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sai_ci_cd_app.ui.theme.Sai_CI_CD_appTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Sai_CI_CD_appTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        AppCenter.start(
+            application, "3a3a062e-be87-41e8-b3f5-2ec722bc9788",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sai_CI_CD_appTheme {
+        Greeting("Android")
+    }
+}
