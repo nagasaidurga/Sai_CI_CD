8b5cbc32320f3b5e474c9f2bf4ee538f7969bd0b
dambati <durga.ambati@yahooinc.com>
2024-06-17 13:24:47 +0530

    added changes

--- app/src/main/java/com/example/sai_ci_cd_app/MainActivity.kt
+++ app/src/main/java/com/example/sai_ci_cd_app/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sai_ci_cd_app.ui.theme.Sai_CI_CD_appTheme
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
@@ -21,6 +27,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppCenter.start(
+            application, "3a3a062e-be87-41e8-b3f5-2ec722bc9788",
+            Analytics::class.java, Crashes::class.java
+        )
         setContent {
             Sai_CI_CD_appTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -31,19 +41,27 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        AppCenter.start(
-            application, "3a3a062e-be87-41e8-b3f5-2ec722bc9788",
-            Analytics::class.java, Crashes::class.java
-        )
+
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        TextField(value = "Username", onValueChange ={} )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            Crashes.generateTestCrash()
+        }) {
+            Text("Click me")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
